dc2c408de4be5159abf6f169eed76b793cb221c1
Jim Menard <jim@jimmenard.com>
2012-07-13 14:24:02 -0400

    diff versions of Emacs use diff file names for dir local settings

---
+++ www/org/.dir-settings.el
@@ -0,0 +1,6 @@
+((org-mode . ((org-publish-project-alist . (("patchmaster"
+                                             :base-directory "."
+                                             :publishing-directory "../public_html"
+                                             :style "<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\"/>"
+                                             :author "Jim Menard"
+                                             :email "jim@jimmenard.com"))))))
