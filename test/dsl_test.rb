a916ce1654781a8b14aa69e16c544aaebdcf569b
Jim Menard <jim@jimmenard.com>
2013-02-22 11:24:08 -0500

    split one big test up into smaller ones

--- test/dsl_test.rb
+++ test/dsl_test.rb
@@ -9,33 +9,40 @@ class DSLTest < Test::Unit::TestCase
     @pm = PM::PatchMaster.instance
     @pm.init_data
     @dsl = PM::DSL.new
+    @dsl.load(EXAMPLE_DSL)
   end
 
   def teardown
     @pm.init_data
   end
 
-  def test_load
-    @dsl.load(EXAMPLE_DSL)
-
+  def test_load_input
     mb = @pm.inputs.detect { |instr| instr.sym == :mb }
     assert_not_nil mb
     assert_kind_of PM::InputInstrument, mb
+  end
 
+  def test_load_output
     kz = @pm.outputs.detect { |instr| instr.sym == :kz }
     assert_kind_of PM::OutputInstrument, kz
+  end
 
+  def test_output_name
     sj = @pm.outputs.detect { |instr| instr.sym == :sj }
     assert_kind_of PM::OutputInstrument, sj
     assert_equal 'MockOutputPort 4', sj.name
+  end
 
-
+  def test_load_triggers
+    mb = @pm.inputs.detect { |instr| instr.sym == :mb }
     triggers = mb.triggers
     assert_equal 5, triggers.length
     trigger = triggers[0]
     assert_equal [PM::CONTROLLER, PM::CC_GEN_PURPOSE_5, 0], trigger.bytes
     assert_equal "{ prev_song }", mb.triggers[3].text
+  end
 
+  def test_load_songs
     assert_equal 2, @pm.all_songs.songs.length
     song = @pm.all_songs.find('First Song')
     assert_kind_of PM::Song, song
@@ -43,12 +50,21 @@ class DSLTest < Test::Unit::TestCase
 
     assert_equal 'First Song', song.name
     assert_equal 2, song.patches.length
+  end
 
+  def test_load_patches
+    song = @pm.all_songs.find('First Song')
     patch = song.patches[0]
     assert_equal [PM::TUNE_REQUEST], patch.start_bytes
     assert_equal [PM::STOP], patch.stop_bytes
     assert_equal 3, patch.connections.length
+  end
 
+  def test_load_connection
+    mb = @pm.inputs.detect { |instr| instr.sym == :mb }
+    kz = @pm.outputs.detect { |instr| instr.sym == :kz }
+    song = @pm.all_songs.find('First Song')
+    patch = song.patches[0]
     conn = patch.connections[0]
 
     assert_equal mb, conn.input
@@ -59,14 +75,24 @@ class DSLTest < Test::Unit::TestCase
     assert_equal 64, conn.pc_prog
     assert_equal (PM::C4..PM::B5), conn.zone
     assert_equal conn.xpose, 12
+  end
 
+  def test_load_another_connection
+    song = @pm.all_songs.find('First Song')
+    patch = song.patches[0]
     conn = patch.connections[1]
+
     assert_equal 2, conn.bank
     assert_equal 100, conn.pc_prog
+  end
 
+  def test_skip_input_chan
     # Make sure that we can skip the second input_chan argument and things
     # are still assigned properly.
+    mb = @pm.inputs.detect { |instr| instr.sym == :mb }
+    sj = @pm.outputs.detect { |instr| instr.sym == :sj }
     conn = @pm.all_songs.find('Second Song').patches[0].connections[0]
+
     assert_equal mb, conn.input
     assert_nil conn.input_chan
     assert_equal sj, conn.output
@@ -75,7 +101,6 @@ class DSLTest < Test::Unit::TestCase
 
   def test_save
     f = '/tmp/dsl_test_save.rb'
-    @dsl.load(EXAMPLE_DSL)
     begin
       @dsl.save(f)
       # TODO write more here
@@ -88,7 +113,6 @@ class DSLTest < Test::Unit::TestCase
 
   def test_what_saves_is_loadable
     f = '/tmp/dsl_test_what_saves_is_loadable.rb'
-    @dsl.load(EXAMPLE_DSL)
     begin
       @dsl.save(f)
       @pm.init_data
@@ -102,7 +126,6 @@ class DSLTest < Test::Unit::TestCase
 
   def test_save_file_contents
     f = '/tmp/dsl_test_save_file_contents.rb'
-    @dsl.load(EXAMPLE_DSL)
     @dsl.save('/tmp/dsl_test_save_file_contents.rb')
     str = IO.read(f)
     assert_match 'output 1, :ws_out, "WaveStation"', str
@@ -117,20 +140,17 @@ class DSLTest < Test::Unit::TestCase
   end
 
   def test_aliases
-    @dsl.load(EXAMPLE_DSL)
     conn = @pm.all_songs.find('Second Song').patches[0].connections[0]
     assert_equal 22, conn.pc_prog
     assert_not_nil conn.zone
   end
 
   def test_zone_takes_empty_end
-    @dsl.load(EXAMPLE_DSL)
     conn = @pm.all_songs.find('Second Song').patches[0].connections[0]
     assert_equal (PM::D4..127), conn.zone
   end
 
   def test_zone_takes_range
-    @dsl.load(EXAMPLE_DSL)
     conn = @pm.all_songs.find('Second Song').patches[0].connections[1]
     assert_equal (PM::C4..PM::B5), conn.zone
   end
@@ -141,6 +161,8 @@ class DSLTest < Test::Unit::TestCase
     str.gsub!(/output 4, :sj/, 'output 4, :ws_out')
     File.open(file, 'w') { |f| f.puts str }
     begin
+      @pm.init_data
+      @dsl = PM::DSL.new
       @dsl.load(file)
       fail "expected unique symbol error to be raised"
     rescue => ex
@@ -151,7 +173,6 @@ class DSLTest < Test::Unit::TestCase
   end
 
   def test_read_filter_text
-    @dsl.load(EXAMPLE_DSL)
     str = <<EOS
 { |connection, bytes|
         if bytes.note_off?
@@ -169,7 +190,6 @@ EOS
   end
 
   def test_messages
-    @dsl.load(EXAMPLE_DSL)
     assert_equal [PM::TUNE_REQUEST], @pm.messages["Tune Request".downcase]
   end
 end
