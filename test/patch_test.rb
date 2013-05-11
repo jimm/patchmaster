f007174aa01ef590dd905701fa3abad4325b481d
Jim Menard <jim@jimmenard.com>
2013-05-11 14:59:56 -0400

    new zone tests

--- test/patch_test.rb
+++ test/patch_test.rb
@@ -60,4 +60,48 @@ class PatchTest < Test::Unit::TestCase
     assert inputs.include?(@in_instrument)
     assert inputs.include?(second_in_instrument)
   end
+
+  def test_all_prog_changes_sent
+    conn2 = PM::Connection.new(@in_instrument, nil, @out_instrument, 3, nil, {:pc_prog => 42})
+    @patch << conn2
+
+    @patch.start
+    assert_equal [PM::PROGRAM_CHANGE + 1, 3, PM::PROGRAM_CHANGE + 2, 42], @out_instrument.port.buffer
+  end
+
+  # Tests two connections with non-overlapping zones that go to different
+  # channels on the same instrument.
+  def test_zones
+    @conn.pc_prog = nil
+    @conn.zone = (0..PM::Gs4)
+
+    conn2 = PM::Connection.new(@in_instrument, nil, @out_instrument, 3, nil)
+    conn2.zone = (PM::A4..127)
+    @patch << conn2
+
+    assert_equal (0..68), @conn.zone
+    assert_equal (69..127), conn2.zone
+
+    @patch.start
+
+    # Send note in @conn1 zone
+    @patch.connections.each { |c| c.midi_in([PM::NOTE_ON + 9, PM::C1, 127]) }
+    expected = [PM::NOTE_ON + 1, PM::C2, 127] # transposed, channel 1
+    assert_equal expected, @out_instrument.port.buffer
+
+    # Send note in conn2 zone
+    @patch.connections.each { |c| c.midi_in([PM::NOTE_ON + 9, PM::C5, 127]) }
+    expected += [PM::NOTE_ON + 2, PM::C5, 127] # not transposed, channel 2
+    assert_equal expected, @out_instrument.port.buffer
+
+    # Note at top of @conn1 zone
+    @patch.connections.each { |c| c.midi_in([PM::NOTE_ON + 9, PM::Gs4, 127]) }
+    expected += [PM::NOTE_ON + 1, PM::Gs5, 127] # transposed, channel 1
+    assert_equal expected, @out_instrument.port.buffer
+
+    # Note at bottom of conn2 zone
+    @patch.connections.each { |c| c.midi_in([PM::NOTE_ON + 9, PM::A4, 127]) }
+    expected += [PM::NOTE_ON + 2, PM::A4, 127] # not transposed, channel 2
+    assert_equal expected, @out_instrument.port.buffer
+  end
 end
